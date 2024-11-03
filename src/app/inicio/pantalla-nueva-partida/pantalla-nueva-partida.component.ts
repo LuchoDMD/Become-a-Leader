5a0896d37657e389e63601e38f384e3c565f3511
piragna1 <gonzalovarelaalagna@gmail.com>
2024-11-03 16:15:23 -0300

    Componente pantalla-nueva-partida

---
+++ src/app/inicio/pantalla-nueva-partida/pantalla-nueva-partida.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-pantalla-nueva-partida',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './pantalla-nueva-partida.component.html',
+  styleUrls: ['./pantalla-nueva-partida.component.css']
+})
+export class PantallaNuevaPartidaComponent {
+  mostrarBruno: boolean = false;
+  mostrarAura: boolean = false;
+  selectedLider: string = '';
+
+  seleccionarLider(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.selectedLider = target.value;
+    console.log('Líder seleccionado:', this.selectedLider); // Agrega un log para verificar
+  }
+}
