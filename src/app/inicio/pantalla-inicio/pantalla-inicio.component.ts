4abcfb34f3ff2edcda17ee03fbc4cd5c68c2aeeb
unknown <gonzalovarelaalagna@gmail.com>
2024-11-05 11:38:01 -0300

    Descripcion de cambios: - organizacion de la base de datos: carpeta 'db' - creacion de carpeta 'servicios' para colocar los distintos servicios - logica para vincular la base de datos con el inicio

--- src/app/inicio/pantalla-inicio/pantalla-inicio.component.ts
+++ src/app/inicio/pantalla-inicio/pantalla-inicio.component.ts
@@ -1,25 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pantalla-inicio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pantalla-inicio.component.html',
   styleUrl: './pantalla-inicio.component.css'
 })
-export class PantallaInicioComponent implements OnInit{
-
-
-  route=inject(Router);
+export class PantallaInicioComponent {
+  idUsuario?:string|null;
+  // route=inject(Router);
   constructor(
     private activatedRoute:ActivatedRoute
-  ){}
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-  NavigateToNewPartida()
-  {
-    this.route.navigate(['nuevaPartida']);
+  ){
+    this.activatedRoute.paramMap.subscribe(
+      (params) => {
+        this.idUsuario = params.get("id");
+      }
+    )
+  };
+  // NavigateToNewPartida()
+  // {
+  //   this.route.navigate(['nuevaPartida']);
+  // }
+  verificarPartida():boolean{
+    //Buscar en la db si el id tiene una partida creada.
+    return false;
   }
 }
