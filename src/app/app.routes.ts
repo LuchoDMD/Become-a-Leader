2d7c5528436ea730dcbcbc5b338f7474b7d63cea
unknown <gonzalovarelaalagna@gmail.com>
2024-11-05 10:20:04 -0300

    unificacion de componentes menuInicio menuCrearPartida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PantallaNuevaPartidaComponent } from './inicio/pantalla-nueva-partida/pantalla-nueva-partida.component';
+import { PantallaInicioComponent } from './inicio/pantalla-inicio/pantalla-inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'Inicio',component:PantallaInicioComponent},
+    {path:'nuevaPartida', component:PantallaNuevaPartidaComponent}
+];
--- src/app/inicio/pantalla-inicio/pantalla-inicio.component.ts
+++ src/app/inicio/pantalla-inicio/pantalla-inicio.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pantalla-inicio',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './pantalla-inicio.component.html',
   styleUrl: './pantalla-inicio.component.css'
 })
-export class PantallaInicioComponent {
+export class PantallaInicioComponent implements OnInit{
 
+
+  route=inject(Router);
+  constructor(
+    private activatedRoute:ActivatedRoute
+  ){}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  NavigateToNewPartida()
+  {
+    this.route.navigate(['nuevaPartida']);
+  }
 }
