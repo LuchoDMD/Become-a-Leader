bbe89a048fef55e8cda78cb8e05f7188c17fc792
unknown <gonzalovarelaalagna@gmail.com>
2024-11-05 12:35:22 -0300

    Descripcion de cambios:
    - Creacion de carpeta 'inicio',
    - Creacion del componente 'alerta',
    - Hardcode para probar la implementacion de la logica de verificacion de partidas existentes del usuario en la base de datos
    - Agregado el proveedor html a app.config.ts
    - Modificacion en app.route.ts en el valor de path
    - Comentario en el componente de la barra de navegacion ('Implementar navegacion de login/registro.')
    - Desarrollo del servicio 'PartidaService'

--- src/app/inicio/pantalla-inicio/pantalla-inicio.component.ts
+++ src/app/inicio/pantalla-inicio/pantalla-inicio.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { PartidaService } from '../../../shared/servicios/partida.service';
 
 @Component({
   selector: 'app-pantalla-inicio',
@@ -10,23 +11,44 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrl: './pantalla-inicio.component.css'
 })
 export class PantallaInicioComponent {
-  idUsuario?:string|null;
+  idUsuario?:string|null="123";
+  tienePartida:boolean=false;
   // route=inject(Router);
   constructor(
-    private activatedRoute:ActivatedRoute
+    private activatedRoute:ActivatedRoute,
+    private servicio:PartidaService
   ){
-    this.activatedRoute.paramMap.subscribe(
-      (params) => {
-        this.idUsuario = params.get("id");
-      }
-    )
+    // this.activatedRoute.paramMap.subscribe(
+    //   (params) => {
+    //     this.idUsuario = params.get("id");
+    //   }
+    // )
+    this.verificarPartida();
   };
   // NavigateToNewPartida()
   // {
   //   this.route.navigate(['nuevaPartida']);
   // }
-  verificarPartida():boolean{
+  verificarPartida(){
     //Buscar en la db si el id tiene una partida creada.
-    return false;
+    if (this.idUsuario){
+      this.servicio.verificarExistenciaPartida(this.idUsuario).subscribe({
+        next:(res)=>{
+          this.tienePartida=res;
+        },
+        error:(res)=>{
+          console.log(res);
+          this.tienePartida=false;
+        }
+      });
+    }
+    else{
+      alert('ID de usuario no encontrado.');
+    }
+  };
+  mensajePregunta(){
+    if (this.tienePartida){
+      
+    }
   }
 }
