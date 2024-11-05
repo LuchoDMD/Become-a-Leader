078a6849c98e143739339bdc348a9dc4ca5318c1
unknown <gonzalovarelaalagna@gmail.com>
2024-11-05 10:40:24 -0300

    Reorganizacion carpeta assets con imagenes utilizadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PantallaInicioComponent } from './inicio/pantalla-inicio/pantalla-inicio.component';
 import { PantallaNuevaPartidaComponent } from './inicio/pantalla-nueva-partida/pantalla-nueva-partida.component';
+import { NavbarComponent } from "../shared/navbar/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PantallaInicioComponent, PantallaNuevaPartidaComponent],
+  imports: [RouterOutlet, PantallaInicioComponent, PantallaNuevaPartidaComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
