7f27ae74abd0f776da7513cd4a709b2ac3acc5e5
unknown <gonzalovarelaalagna@gmail.com>
2024-11-05 12:41:34 -0300

    Cambios:
    - Creacion componente 'alerta'

--- src/app/inicio/alerta/alerta.component.ts
+++ src/app/inicio/alerta/alerta.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+// alerta.component.ts
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-alerta',
-  standalone: true,
-  imports: [],
   templateUrl: './alerta.component.html',
-  styleUrl: './alerta.component.css'
+  styleUrls: ['./alerta.component.css']
 })
 export class AlertaComponent {
+  @Input() mensaje: string = '¿Estás seguro de que deseas sobrescribir la partida existente?';
+  @Input() mostrar: boolean = false;
+  @Output() onConfirmar = new EventEmitter<void>();
+  @Output() onCancelar = new EventEmitter<void>();
 
+  confirmar() {
+    this.onConfirmar.emit();
+  }
+
+  cancelar() {
+    this.onCancelar.emit();
+  }
 }
