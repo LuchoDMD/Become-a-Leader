d7b143836447395a376b6c01c3d0bed18f8f89be
LuchoDMD <lucio.mdq21@gmail.com>
2024-11-08 23:44:29 -0300

    Probando servicio PokeAPI

---
+++ src/app/Batalla/Interfaces/estadisticas.ts
@@ -0,0 +1,9 @@
+export interface Estadisticas 
+{
+    hp:number, //Vida
+    atk:number, //Ataque Fisico
+    def:number, //Defensa Fisica
+    satk:number, //Ataque Especial
+    sdef:number, //Defensa Especial
+    spd:number  //Velocidad
+}
---
+++ src/app/Batalla/Interfaces/movimientos.ts
@@ -0,0 +1,9 @@
+export interface Movimientos 
+{
+    id:string,
+    nombre:string,
+    tipo:string,
+    potencia:number,
+    precision:number,
+    pp:number //Cantidad de Usos
+}
