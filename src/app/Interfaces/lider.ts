faf82d9e1c9a419d6fc8e0c14d4132c6109d8ae9
LuchoDMD <lucio.mdq21@gmail.com>
2024-11-10 00:42:46 -0300

    Editando Lider

--- src/app/Interfaces/lider.ts
+++ src/app/Interfaces/lider.ts
@@ -1,10 +1,8 @@
-import { UUID } from "crypto";
 import { Pokemon } from "./pokemon";
 
 export interface Lider 
 {
     id:string,
     nombre:string,
-    genero:string, //chico - chica es mas que nada para que defina la imagen a renderizar
-    equipo:string[] //Min:2 Max:6 almacena los id // nombres
+    equipo:Pokemon[] //Min:2 Max:6 almacena los id // nombres
 }
