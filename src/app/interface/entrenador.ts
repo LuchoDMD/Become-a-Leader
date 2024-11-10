7fcf0781c2ae65b21321b96ea99cfb6beec47abe
Varela97 <quimeyvarela97@gmail.com>
2024-11-10 15:44:32 -0300

    Eliminar archivos innecesarios

--- src/app/interface/entrenador.ts
+++ src/app/interface/entrenador.ts
@@ -1,6 +1,5 @@
 
 import { Pokemon } from "./pokemon";
-import { Item } from "./item";
 
 export interface Entrenador {
     id:string,
--- src/app/interface/item.ts
+++
@@ -1,7 +0,0 @@
-export interface Item {
-    id?:number,
-    nombre:string,
-    categoria:string,
-    efecto:string,
-    precio:Map<string,number>, //{ compra:number, venta:number }
-}
