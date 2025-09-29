38e82f0e5ed08e0ecae7414de283c260e537ec8e
LuchoDMD <lucio.mdq21@gmail.com>
2025-09-29 17:09:27 -0300

    Quitando Componente Ruta
    Pense en mejor no utilizar el concepto de ruta y captura, prefiriendo optar por la opcion de una tienda-pokemon con la cual puedes adquirir pokemons para mejorar tu equipo inicial

---
+++ src/app/components/tienda-pokemon/tienda-pokemon.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tienda-pokemon',
+  imports: [],
+  templateUrl: './tienda-pokemon.html',
+  styleUrl: './tienda-pokemon.css'
+})
+export class TiendaPokemon {
+
+}
